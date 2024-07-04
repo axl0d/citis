f596ce07d773a24cd02fa7eb6f2f63982b94c0a8
Axl Orellana <axlod95@gmail.com>
2024-07-04 06:36:14 -0500

    move workshop detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:citis/workshop.dart';
 import 'package:flutter/material.dart';
 
+import 'pages/workshop_detail_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -119,62 +121,8 @@ class _WorkshopCard extends StatelessWidget {
         onTap: () => Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => _WorkshopDetailPage(workshop: workshop),
+            builder: (context) => WorkshopDetailPage(workshop: workshop),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class _WorkshopDetailPage extends StatelessWidget {
-  const _WorkshopDetailPage({
-    required this.workshop,
-  });
-
-  final Workshop workshop;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: workshop.topic.color,
-        title: const Text("Workshop"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Título",
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 4),
-            Text(
-              workshop.title,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 4),
-            Row(
-              children: [
-                const Icon(Icons.person_outlined),
-                const SizedBox(width: 4),
-                Text(workshop.speaker.fullTitle),
-              ],
-            ),
-            const SizedBox(height: 4),
-            Text(
-              "Categoría",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-            Chip(
-              label: Text(workshop.topic.inSpanish),
-              backgroundColor: workshop.topic.color,
-            ),
-            const SizedBox(height: 4),
-            Text(workshop.location),
-          ],
         ),
       ),
     );
