4f5707b77b471b83693de6ae8f90582d1f1597e9
Axl Orellana <axlod95@gmail.com>
2024-07-04 18:20:42 -0500

    update dump data

--- lib/dump_data.dart
+++ lib/dump_data.dart
@@ -10,53 +10,154 @@ const speaker5 = Speaker(name: 'Aura Liz Cevallos Almeida', degree: 'MSc');
 const speaker6 =
     Speaker(name: 'Marcelo Xavier Estrella Guayasamin', degree: 'MSc');
 const speaker7 = Speaker(name: 'Dario Fernando Huilcapi Subia', degree: 'MSc');
+const speaker8 = Speaker(name: 'Héctor René Vega Carrillo', degree: 'Ph.D');
 
-const workshops = [
-  Workshop(
-    speaker: speaker1,
-    topic: TechnicalTrack.sustainability,
-    location: 'room B1',
-    title:
-        'Fermentation process and industrial waste upcycling by industrial microorganisms',
+const workshops = [];
+
+final jul17 = [
+  const EventTrack(hour: "8:00", track: "Registration to the conference"),
+  const EventTrack(
+    hour: "09:00 - 13:00",
+    workshops: [
+      Workshop(
+        speaker: speaker1,
+        topic: TechnicalTrack.sustainability,
+        location: 'room B1',
+        title:
+            'Fermentation process and industrial waste upcycling by industrial microorganisms',
+      ),
+      Workshop(
+        speaker: speaker2,
+        topic: TechnicalTrack.industry,
+        location: 'Computer Lab. A7 -  2nd floor building A',
+        title:
+            'Exploring Industrial and Commercial Microgrids: Regulation and Opportunities for the Energy Future',
+      ),
+      Workshop(
+        speaker: speaker3,
+        topic: TechnicalTrack.mobility,
+        location: 'Computer Lab. A4 -  2nd floor building A',
+        title:
+            'Estimation of pollutant emissions in real driving conditions using OBD, GPS data, and Machine Learning',
+      ),
+      Workshop(
+        speaker: speaker4,
+        topic: TechnicalTrack.infoTelecom,
+        location: 'Computer Lab. A2 -  2nd floor building A',
+        title: 'Creating data plots in Latex using the pgfplots package',
+      ),
+    ],
   ),
-  Workshop(
-    speaker: speaker2,
-    topic: TechnicalTrack.industry,
-    location: 'Computer Lab. A7 -  2nd floor building A',
-    title:
-        'Exploring Industrial and Commercial Microgrids: Regulation and Opportunities for the Energy Future',
+  const EventTrack(
+    hour: "13:00",
+    track: "Registration Bureau (hall next to the Aula Magna)",
   ),
-  Workshop(
-    speaker: speaker3,
-    topic: TechnicalTrack.mobility,
-    location: 'Computer Lab. A4 -  2nd floor building A',
-    title:
-        'Estimation of pollutant emissions in real driving conditions using OBD, GPS data, and Machine Learning',
+  const EventTrack(
+    hour: "13:00 - 14:00",
+    track: "LUNCH - 6th floor",
   ),
-  Workshop(
-    speaker: speaker4,
-    topic: TechnicalTrack.infoTelecom,
-    location: 'Computer Lab. A2 -  2nd floor building A',
-    title: 'Creating data plots in Latex using the pgfplots package',
+  const EventTrack(
+    hour: "14:00 - 18:00",
+    workshops: [
+      Workshop(
+        speaker: speaker5,
+        topic: TechnicalTrack.industry,
+        location: 'Computer Lab. A7 -  2nd floor building A',
+        title: 'TICs: The future of learning',
+      ),
+      Workshop(
+        speaker: speaker6,
+        topic: TechnicalTrack.mobility,
+        location: 'Computer Lab. A4 -  2nd floor building A',
+        title:
+            'Dry cell electrolyzer design for HHO gas production: An alternative for Sustainable Mobility',
+      ),
+      Workshop(
+        speaker: speaker7,
+        topic: TechnicalTrack.infoTelecom,
+        location: 'Computer Lab. A2 -  2nd floor building A',
+        title:
+            'Principles of information security and risk management to mitigate threats that commonly affect businesses',
+      ),
+    ],
   ),
-  Workshop(
-    speaker: speaker5,
-    topic: TechnicalTrack.industry,
-    location: 'Computer Lab. A7 -  2nd floor building A',
-    title: 'TICs: The future of learning',
+];
+
+final jul18 = [
+  const EventTrack(hour: "8:00", track: "Registration to the conference"),
+  const EventTrack(
+    hour: "09:00 - 17:00",
+    track: "Feria de innovación tecnológica - Detras del aula magna",
   ),
-  Workshop(
-    speaker: speaker6,
-    topic: TechnicalTrack.mobility,
-    location: 'Computer Lab. A4 -  2nd floor building A',
-    title:
-        'Dry cell electrolyzer design for HHO gas production: An alternative for Sustainable Mobility',
+  const EventTrack(
+    hour: "09:30 - 10:30",
+    track: "Inaugural ceremony - AULA MAGNA",
   ),
-  Workshop(
-    speaker: speaker7,
-    topic: TechnicalTrack.infoTelecom,
-    location: 'Computer Lab. A2 -  2nd floor building A',
-    title:
-        'Principles of information security and risk management to mitigate threats that commonly affect businesses',
+  const EventTrack(
+    hour: "10:30 - 11:30",
+    workshop: Workshop(
+      speaker: speaker8,
+      topic: TechnicalTrack.industry,
+      location: "AULA MAGNA",
+      title: "Energy, Environment, and Society",
+    ),
+  ),
+  const EventTrack(
+    hour: "12:00 - 13:00",
+    workshops: [
+      Workshop(
+        speaker: speaker5,
+        topic: TechnicalTrack.industry,
+        location: 'Computer Lab. A7 -  2nd floor building A',
+        title: 'TICs: The future of learning',
+      ),
+      Workshop(
+        speaker: speaker6,
+        topic: TechnicalTrack.mobility,
+        location: 'Computer Lab. A4 -  2nd floor building A',
+        title:
+            'Dry cell electrolyzer design for HHO gas production: An alternative for Sustainable Mobility',
+      ),
+      Workshop(
+        speaker: speaker7,
+        topic: TechnicalTrack.infoTelecom,
+        location: 'Computer Lab. A2 -  2nd floor building A',
+        title:
+            'Principles of information security and risk management to mitigate threats that commonly affect businesses',
+      ),
+    ],
+  ),
+  const EventTrack(
+    hour: "13:00",
+    track: "Registration Bureau (hall next to the Aula Magna)",
+  ),
+  const EventTrack(
+    hour: "13:00 - 14:00",
+    track: "LUNCH - 6th floor",
+  ),
+  const EventTrack(
+    hour: "14:00 - 18:00",
+    workshops: [
+      Workshop(
+        speaker: speaker5,
+        topic: TechnicalTrack.industry,
+        location: 'Computer Lab. A7 -  2nd floor building A',
+        title: 'TICs: The future of learning',
+      ),
+      Workshop(
+        speaker: speaker6,
+        topic: TechnicalTrack.mobility,
+        location: 'Computer Lab. A4 -  2nd floor building A',
+        title:
+            'Dry cell electrolyzer design for HHO gas production: An alternative for Sustainable Mobility',
+      ),
+      Workshop(
+        speaker: speaker7,
+        topic: TechnicalTrack.infoTelecom,
+        location: 'Computer Lab. A2 -  2nd floor building A',
+        title:
+            'Principles of information security and risk management to mitigate threats that commonly affect businesses',
+      ),
+    ],
   ),
 ];
