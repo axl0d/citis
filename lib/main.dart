a89df2ab14f3b729d1b68c12618ca3d69e898174
Axl Orellana <axlod95@gmail.com>
2024-07-04 06:33:05 -0500

    add workshop detail page

--- lib/main.dart
+++ lib/main.dart
@@ -84,36 +84,97 @@ class _WorkshopCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: ListTile(
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Workshop',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(workshop.title),
-          ],
-        ),
-        subtitle: RichText(
-          text: TextSpan(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        child: ListTile(
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const TextSpan(
-                text: 'Ponente:',
+              const Text(
+                'Workshop',
                 style: TextStyle(fontWeight: FontWeight.bold),
               ),
-              const WidgetSpan(child: SizedBox(width: 4)),
-              TextSpan(
-                text: '${workshop.speaker.name}, ${workshop.speaker.degree}',
-              ),
+              Text(workshop.title),
             ],
-            style: const TextStyle(color: Colors.black),
+          ),
+          subtitle: RichText(
+            text: TextSpan(
+              children: [
+                const TextSpan(
+                  text: 'Ponente:',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const WidgetSpan(child: SizedBox(width: 4)),
+                TextSpan(text: workshop.speaker.fullTitle),
+              ],
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+          trailing: IconButton(
+            visualDensity: VisualDensity.compact,
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
           ),
         ),
-        trailing: IconButton(
-          visualDensity: VisualDensity.compact,
-          icon: const Icon(Icons.arrow_forward_ios),
-          onPressed: () {},
+        onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => _WorkshopDetailPage(workshop: workshop),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _WorkshopDetailPage extends StatelessWidget {
+  const _WorkshopDetailPage({
+    required this.workshop,
+  });
+
+  final Workshop workshop;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: workshop.topic.color,
+        title: const Text("Workshop"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Título",
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 4),
+            Text(
+              workshop.title,
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 4),
+            Row(
+              children: [
+                const Icon(Icons.person_outlined),
+                const SizedBox(width: 4),
+                Text(workshop.speaker.fullTitle),
+              ],
+            ),
+            const SizedBox(height: 4),
+            Text(
+              "Categoría",
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Chip(
+              label: Text(workshop.topic.inSpanish),
+              backgroundColor: workshop.topic.color,
+            ),
+            const SizedBox(height: 4),
+            Text(workshop.location),
+          ],
         ),
       ),
     );
