69c966ab7f94cdf0b79aa4cc99311e1ff44936f2
Axl Orellana <axlod95@gmail.com>
2024-07-06 13:50:25 -0500

    redesign sign page

--- lib/pages/sign_page.dart
+++ lib/pages/sign_page.dart
@@ -1,32 +1,62 @@
 import 'package:citis/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
+import '../theme.dart';
+
 class SignPage extends StatelessWidget {
   const SignPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFcf2e2e),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Image.asset("assets/logo.png"),
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/white_background.png"),
           ),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const HomePage(),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Center(
+              child: Image.asset(
+                "assets/ups_logo.png",
+                width: 200,
               ),
             ),
-            child: const Text("Iniciar sesión con Google"),
-          ),
-        ],
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Image.asset(
+                  "assets/red_citis_logo.png",
+                  width: 300,
+                  height: 400,
+                ),
+              ),
+            ),
+            Center(
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: r3,
+                  foregroundColor: Colors.white,
+                ),
+                icon: const Icon(Icons.arrow_forward_ios),
+                iconAlignment: IconAlignment.end,
+                label: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("Registrarse"),
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
