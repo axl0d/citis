d5384a5f1c1b8ceae57b16ee02ddd9d13240e686
Axl Orellana <axlod95@gmail.com>
2024-07-07 20:38:22 -0500

    redesign home view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:citis/dumb_data.dart';
 import 'package:citis/models.dart';
 import 'package:citis/widgets/full_agenda_body.dart';
 import 'package:flutter/material.dart';
@@ -14,9 +15,111 @@ class HomePage extends StatefulWidget {
 }
 
 const bodies = [
-  Center(child: Text('Home')),
+  _HomeView(),
   _AgendaView(),
 ];
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+    final workshops = jul17[1].workshops!;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Gap(8),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.5,
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              children: [
+                Card(
+                  color: r2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Image.asset("assets/white_ups_logo.png"),
+                  ),
+                ),
+                Card(
+                  color: primary,
+                  child: Image.asset("assets/white_ups_logo.png"),
+                ),
+                Card(
+                  color: primary,
+                  child: Image.asset("assets/white_ups_logo.png"),
+                ),
+                Card(
+                  color: r3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Image.asset("assets/logo.png"),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8,
+            ),
+            child: Text(
+              "Next Session",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ),
+          const Divider(),
+          for (final workshop in workshops) ...[
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: NextSessionItem(workshop: workshop),
+            ),
+            const Divider(),
+          ]
+        ],
+      ),
+    );
+  }
+}
+
+class NextSessionItem extends StatelessWidget {
+  const NextSessionItem({
+    super.key,
+    required this.workshop,
+  });
+
+  final Workshop workshop;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(workshop.title),
+        const Gap(8),
+        Text(
+          workshop.speaker.fullTitle,
+        ),
+        const Gap(8),
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Jul 17",
+            ),
+            Text(
+              "Salon 123",
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
